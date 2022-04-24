3ff9a04cd0c4ff6e9279c4af2dca3435c3f0c71c
Sümeyye Akkaş <42119724+oluroyleseyler@users.noreply.github.com>
2022-04-24 14:15:09 +0300

    Update api.py
    comments added

--- backend/api.py
+++ backend/api.py
@@ -7,6 +7,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+#cors confirigations
+
 origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
@@ -42,12 +44,10 @@ with open ("massive_data.json" , 'r') as f:
 async def all_genes ():
     return genetic    
 
+#classification filter endpoint
+
 @app.get('/api/{c_classification}', status_code=200)
 def get_gene (c_classification):
     gene = [c for c  in genetic if c['classification'] == c_classification]
     return gene  if len (gene) > 0 else {}    
 
-@app.get('/api/classification')
-def all_classification ():
-    classification =genetic["classification"]
-    return classification
